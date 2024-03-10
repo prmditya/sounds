5746e035dd01580b89ac98f7c9dda103e47bad46
Thoriq Kusuma <thoriqk2@gmail.com>
2024-03-10 15:16:03 +0700

    feat(ui): add section-1 (about) ui

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,15 @@
-import { Hero, Navbar } from "./components";
+import { Hero, Navbar, About } from "./components";
 
 function App() {
   return (
     <>
       <Navbar />
-      <div className="container mx-auto">
-        <Hero />
+
+      <div className="container-padding px-[15px] xl:px-0">
+        <div className="container mx-auto">
+          <Hero />
+          <About />
+        </div>
       </div>
     </>
   );
